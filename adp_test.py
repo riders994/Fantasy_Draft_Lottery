0cfca70f27cb15a29b2d827a9be4a46aa4fc3cda
Rohan Vahalia <r.vahalia@gmail.com>
2019-08-14 23:45:16 -0500

    Improved draft script a little, added documentation, added requirements file.

--- adp_test.py
+++ adp_test.py
@@ -2,29 +2,31 @@ import numpy as np
 import pandas as pd
 
 
-PLAYERS = 'a,b,c,d,e,f,g,h,i,j'.split(',')
+"""
+This is a dumb average-draft-position simulator. I'm sure I could compute the
+probability given enough time (and ignoring pick preference) but honestly this
+was way easier. Runs 10k simulations of a 10 man and 12 man draft with the provided
+odds. Based on my other lottery script, but stripped down (slightly). Can be
+expanded to take other arrangements pretty easily.
+"""
+
+
+PLAYERS_10 = 'a,b,c,d,e,f,g,h,i,j'.split(',')
+PLAYERS_12 = 'a,b,c,d,e,f,g,h,i,j,k,l'.split(',')
 ODDS_10 = [125, 120, 115, 110, 105, 95, 90, 85, 80, 75]
 ODDS_12 = [116, 111, 105, 98, 92, 86, 80, 74, 68, 63, 57, 50]
-
-ball_pit = np.arange(1000)
-
-np.random.shuffle(ball_pit)
 
 LOTTERIED = 6
 
 
-def adp_sim(num=10, odds=ODDS_10):
+def adp_sim(odds=ODDS_10, players=PLAYERS_10):
     ball_pit = np.arange(1000)
     np.random.shuffle(ball_pit)
     selected_players = set()
     player_pick_dict = {}
-    players = PLAYERS
     pick = 1
     i = 0
-    if num == 12:
-        players += ['k', 'l']
-
-    combos = np.array(sum([[players[j]] * odds[j] for j in range(num)], []))
+    combos = np.array(sum([[players[j]] * odds[j] for j in range(len(odds))], []))
 
     while pick <= LOTTERIED:
         ball_drawn = ball_pit[i]
@@ -48,12 +50,12 @@ def build_df(simulation):
     return pd.DataFrame.from_dict(simulation).reset_index(drop=True)
 
 
-def adp_chart(num=10, odds=ODDS_10, trials=10000):
-    trials = [build_df(adp_sim(num, odds)) for i in range(trials)]
+def adp_chart(odds=ODDS_10, players=PLAYERS_10, trials=10000):
+    trials = [build_df(adp_sim(odds, players)) for i in range(trials)]
     full_df = pd.concat(trials)
     print(full_df.mean(0))
 
 
 if __name__ == '__main__':
     adp_chart()
-    adp_chart(12, ODDS_12)
+    adp_chart(ODDS_12, PLAYERS_12)
--- messagesender.py
+++ messagesender.py
@@ -4,13 +4,29 @@ from fbchat import models
 
 
 class FacebookMessenger:
+    """
+    This is a class that I use to send the lottery outcome of my fantasy basketball
+    draft directly to a facebook messenger thread. I do this to guarantee that
+    the chain of custody of information is completely sanitary (or as sanitary as
+    is within reason).
+    """
+
+    # I think this tells the chat client that it's a group-chat
     thread_type = models.ThreadType.GROUP
 
     def __init__(self, creds):
+        """
+        :param creds: dict. Holds credentials, and thread information for chats.
+                      Does not utilize .get() method specifically to error out.
+        """
         self.thread_id = creds['thread']
         self.chat_client = Client(creds['email'], creds['pw'])
 
     def send_message(self, message):
+        """
+        This is the method that sends messages.
+        :param message: str. Message to be sent.
+        """
         self.chat_client.send(models.Message(text=message), thread_id=self.thread_id, thread_type=self.thread_type)
 
 
@@ -18,5 +34,5 @@ if __name__ == '__main__':
     with open('creds.json', 'rb') as file:
         credentials = json.load(file)
 
-    # test = FacebookMessenger(credentials)
-    # test.send_message('lol butts lol')
+    test = FacebookMessenger(credentials)
+    test.send_message('lol butts lol')
