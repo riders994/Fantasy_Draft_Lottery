dac9f732d03a79f0774fe27eb0e683c8daeac4a2
Rohan Vahalia <r.vahalia@gmail.com>
2020-11-23 00:32:56 -0600

    Add tests, license, update setup.py

---
+++ tests/test_draftorder.py
@@ -0,0 +1,27 @@
+import pytest
+from fantasydraftlottery import Lottery
+
+
+PLAYERS_DICT = {
+    "players": {
+        "Alex K": [120, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]],
+        "Alison": [75,  [1, 2, 3, 4, 5, 10, 9, 8, 7, 6]],
+        "Alex W": [100, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]],
+        "Chris":  [90,  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]],
+        "James":  [100, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]],
+        "John":   [110, [3, 10, 9, 8, 7, 6, 2, 1, 4, 5]],
+        "Neil":   [125, [4, 2, 3, 1, 5, 6, 7, 8, 9, 10]],
+        "Ravi":   [85,  [4, 5, 6, 7, 3, 2, 1, 8, 9, 10]],
+        "Rohan":  [115, [1, 2, 3, 4, 5, 6, 10, 9, 8, 7]],
+        "Sahil":  [80,  [6, 7, 10, 8, 4, 3, 5, 1, 2, 9]]
+    },
+    "placement": "Ravi,Chris,Rohan,John,Neil,Alex K,Alex W,James,Sahil,Alison"
+}
+LOTTERIED = 4
+
+lotto = Lottery()
+lotto.run(PLAYERS_DICT, LOTTERIED)
+
+
+def test_lotto_size():
+    assert len(lotto.pick_messages)/2 == len(PLAYERS_DICT['players'])
